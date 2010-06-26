5ea75b7c4f47f3d051898a3210883a2ad8911ef5
fallwith <fallwith@gmail.com>
2010-06-26 00:51:58 -0700

    xml helpers: added 'element' helper (give me back the first element with the name foo) and 'attribute_value' helper (give me the value for the attribute foo)

--- lib/blindfold/xml/monkey.rb
+++ lib/blindfold/xml/monkey.rb
@@ -3,6 +3,14 @@ module Nokogiri::XML
     def elements(name)
       xpath("//#{name}")
     end
+    def element(name)
+      elements(name).first
+    end
+  end
+  class Element
+    def attribute_value(name)
+      attribute(name).value
+    end
   end
   class NodeSet
     def with_attributes(attributes)
