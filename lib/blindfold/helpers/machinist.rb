6abe1b2aa303d421d5f6ca44c4153840200fdaf4
fallwith <fallwith@gmail.com>
2010-06-26 20:48:09 -0700

    adding blueprint reloading fix to Machinist helper class until the Machinist / Rack::Test http request AR bug can be tracked down

--- lib/blindfold/helpers/machinist.rb
+++ lib/blindfold/helpers/machinist.rb
@@ -2,6 +2,18 @@ module HelperMethods
   # Spawn n blueprint driven instances of a model
   # NOTE: Machinist2 makes this easy enough to do with 'make' itself
   def spawn(name, args={})
+    
+    # Reload Machinist blueprints despite them having been loaded earlier.
+    # For some reason, Rack::Test's use of ActionController::Dispatcher.call(env)
+    # causes the blueprints to fall out of scope for the next Connection instance.
+    #
+    # TODO: determine root cause, raise ticket if necessary with Rack::Test
+    #
+    blueprints_dir = File.join(Blindfold.config_dir, Launcher.blueprints_dir)
+    if Dir.exists?(blueprints_dir)
+      Dir[File.join(blueprints_dir,'**','*_blueprint.rb')].each {|f| load f}
+    end
+    
     klass = name.to_s.singularize.camelize.constantize
     # klass.make(args)
     OpenStruct.new(klass.make(args).attributes)
